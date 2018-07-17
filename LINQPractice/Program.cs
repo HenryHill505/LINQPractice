ab97d24e1a1396e6880d38adddbce5548e371d18
HenryHill505 <hhill505@gmail.com>
2018-07-17 10:04:58 -0500

    Completed problem 1

--- LINQPractice/Program.cs
+++ LINQPractice/Program.cs
@@ -10,6 +10,18 @@ namespace LINQPractice
     {
         static void Main(string[] args)
         {
+            //Problem 1
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+            var resultList = words.Where(w => w.Contains("th"));
+
+            foreach(string result in resultList)
+            {
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+
+            //Problem 2
         }
     }
 }
