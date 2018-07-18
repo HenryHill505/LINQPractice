b1973c149f67a5165640f8820f1da482429fd4cf
HenryHill505 <hhill505@gmail.com>
2018-07-18 08:08:20 -0500

    Arranged code into actual functions and deleted comments

--- LINQPractice/Program.cs
+++ LINQPractice/Program.cs
@@ -12,53 +12,63 @@ namespace LINQPractice
         {
             //Problem 1
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-
-            var resultList = words.Where(w => w.Contains("th"));
-
-            foreach(var result in resultList)
-            {
-                Console.WriteLine(result);
-            }
-            Console.ReadLine();
-
-            //Problem 2
             List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
-
-            var singleNameList = names.Distinct();
-
-            foreach (var result in singleNameList)
-            {
-                Console.WriteLine(result);
-            }
-            Console.ReadLine();
-
-            //Problem 3
             List<string> classGrades = new List<string>()
                     {
-                    "80,100,92,89,65", //90.25
-                    "93,81,78,84,69", //84
-                    "73,88,83,99,64", //85.75
-                    "98,100,66,74,55" //84.5
-                    //86.125
+                    "80,100,92,89,65",
+                    "93,81,78,84,69", 
+                    "73,88,83,99,64", 
+                    "98,100,66,74,55" 
                     };
-
-            var classGradesAsNunmbers = classGrades.Select(g => g.Split(',').Select(m => double.Parse(m)));
-            var classGradesLowestDropped = classGradesAsNunmbers.Select(g => g.Where(m => m != g.Min()));
-            var classGradeAverages = classGradesLowestDropped.Select(g => g.Average());
-            Console.WriteLine(classGradeAverages.Average());
-            
-
-            //Problem 4
-
             string testString = "Terrill";
 
-            var letterAndFrequency = testString.ToUpper().GroupBy(l => l).OrderBy(l => l.Key).Select(l => new { character = l.Key, count = l.Count() });
 
-            foreach (var letter in letterAndFrequency)
+            SearchWordsForTH(words);
+            RemoveDuplicateStrings(names);
+            AverageClassGrades(classGrades);
+            GetLetterFrequency(testString);
+
+            void SearchWordsForTH(List<string> wordList) 
+             {
+
+                var resultList = wordList.Where(w => w.Contains("th"));
+                foreach (var result in resultList)
+                {
+                    Console.WriteLine(result);
+                }
+                Console.ReadLine();
+             }
+
+            void RemoveDuplicateStrings(List<string> nameList)
             {
-                Console.WriteLine(letter.character+letter.count.ToString());
+                var singleNameList = nameList.Distinct();
+
+                foreach (var result in singleNameList)
+                {
+                    Console.WriteLine(result);
+                }
+                Console.ReadLine();
             }
-            Console.ReadLine();
+
+            void AverageClassGrades(List<string> gradeList)
+            {
+                var classGradesAsNumbers = gradeList.Select(g => g.Split(',').Select(m => double.Parse(m)));
+                var classGradesLowestDropped = classGradesAsNumbers.Select(g => g.Where(m => m != g.Min()));
+                var classGradeAverages = classGradesLowestDropped.Select(g => g.Average());
+                Console.WriteLine(classGradeAverages.Average());
+                Console.ReadLine();
+            }
+ 
+            void GetLetterFrequency(string input)
+            {
+                var letterAndFrequency = input.ToUpper().GroupBy(l => l).OrderBy(l => l.Key).Select(l => new { character = l.Key, count = l.Count() });
+
+                foreach (var letter in letterAndFrequency)
+                {
+                    Console.WriteLine(letter.character + letter.count.ToString());
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
